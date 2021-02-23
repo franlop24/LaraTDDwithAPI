e4761acca4fda751287b9f3239c66853cc192496
Francisco Lopez <franlopbri@gmail.com>
2021-02-23 15:10:42 -0600

    programando prueba show valida y status 404

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Http\Controllers\Api;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Post;
 
 class PostControllerTest extends TestCase
 {
@@ -31,5 +32,23 @@ class PostControllerTest extends TestCase
 
         $response->assertStatus(422) // Estatus HTTP 422
             ->assertJsonValidationErrors('title');
+    }
+
+    public function test_show()
+    {
+        $post = factory(Post::class)->create();
+
+        $response = $this->json('GET', "/api/posts/$post->id");
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+            ->assertJson(['title' => $post->title])
+            ->assertStatus(200);
+
+    }
+
+    public function test_404_show()
+    {
+        $response = $this->json('GET', "/api/posts/100000");
+        $response->assertStatus(404);
     }
 }
